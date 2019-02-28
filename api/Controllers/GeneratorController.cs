5df81738535a3b913a973a829a318eb025f319bf
Ali Shahrooz <rainz@stree.co>
2019-02-28 16:54:31 -0500

    updates

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -22,7 +22,7 @@ namespace api.Controllers
         public async Task EmailRandomNames(Range range, string email = "test@fake.com")
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Generator", "generator1@generate.com"));
+            message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
             message.To.Add(new MailboxAddress("", email));
             message.Subject = "Here are your random names!";
 
