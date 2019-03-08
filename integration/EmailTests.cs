f616a27a3844df51ddc20a2d9f867b307a5010de
Ali Shahrooz <rainz@stree.co>
2019-03-08 14:56:20 -0500

    updates

--- integration/EmailTests.cs
+++ integration/EmailTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -18,91 +19,88 @@ namespace integration
         public const string GeneratorApiRoot = "http://generator";
         public const string MailHogApiV2Root = "http://mail:8025/api/v2";
 
-        //public const string GeneratorApiRoot = "http://localhost:8086";
+        //public const string GeneratorApiRoot = "http://localhost:49551";
         //public const string MailHogApiV2Root = "http://localhost:8025/api/v2";
+
+        public static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //services.AddLogging(c => c.AddConsole());
+            //services.AddSingleton<IFileSystem, FileSystem>();
+            //services.AddSingleton<IMarkdownToHtml, MarkdownToHtml>();
+
+            //services.AddHttpClient<IBasketService, BasketService>()
+            //        .SetHandlerLifetime(TimeSpan.FromMinutes(5))  //Set lifetime to five minutes
+            //        .AddPolicyHandler(GetRetryPolicy());
+
+            services.AddHttpClient("Generic", client =>
+            {
+                //client.BaseAddress = new Uri("http://localhost:49551");
+                //client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            })
+            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10)
+            }, (result, timeSpan, retryCount, context) =>
+            {
+                Console.WriteLine($"Could not connect. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+                Debug.WriteLine($"Could not connect. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+                //logger.Warning($"Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+                //Console.WriteLine($"Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+            }));
+
+            return services.BuildServiceProvider();
+        }
 
         [Fact]
         public async Task SendEmailWithNames_IsFromGenerator()
         {
-            //var httpClient = new HttpClient();
-
-
-
-            //var builder = new HostBuilder()
-            //    .ConfigureServices((hostContext, services) =>
-            //    {
-            //        //IPolicyRegistry<string> registry = services.AddPolicyRegistry();
-
-            //        //IAsyncPolicy<HttpResponseMessage> httWaitAndpRetryPolicy =
-            //        //    Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-            //        //        .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt));
-
-            //        //registry.Add("SimpleWaitAndRetryPolicy", httWaitAndpRetryPolicy);
-
-            //        //IAsyncPolicy<HttpResponseMessage> noOpPolicy = Policy.NoOpAsync()
-            //        //    .AsAsyncPolicy<HttpResponseMessage>();
-
-            //        //registry.Add("NoOpPolicy", noOpPolicy);
-
-            //        services.AddHttpClient("GitHub", x =>
-            //        {
-            //            x.BaseAddress = new Uri("https://api.github.com/");
-            //            x.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
-            //        }).AddTransientHttpErrorPolicy(b => b.WaitAndRetryAsync(new[]
-            //        {
-            //            TimeSpan.FromSeconds(1),
-            //            TimeSpan.FromSeconds(5),
-            //            TimeSpan.FromSeconds(10)
-            //        }));
-            //    });
-
-            //        //setup our DI
-            //        var serviceProvider = new ServiceCollection()
-            //            //.AddSingleton<IFooService, FooService>()                
-            //            //.AddSingleton<IBarService, BarService>()
-            //        .AddHttpClient("GitHub", client =>
-            //{
-            //    client.BaseAddress = new Uri("https://api.github.com/");
-            //    client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
-            //})
-
-            //.AddHttpClient("MailServer", x =>
-            //{
-            //    x.BaseAddress = new Uri("https://api.github.com/");
-            //    //x.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
-            //})
-
-            //.BuildServiceProvider();
-
-            //Console.WriteLine("WAITING 10 SECS");
-            //System.Threading.Thread.Sleep(10000);
-
             // send email
-            var client = new HttpClient();
+            var provider = ConfigureServices();
+
+            var _httpClientFactory = provider.GetService<IHttpClientFactory>();
+
+            var client = _httpClientFactory.CreateClient("Generic");
+
+            //var names = client.GetStringAsync("/Names").Result;
+
+
+
+            //var client = new HttpClient();
             var sendEmail = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
                 RequestUri = new Uri($"{GeneratorApiRoot}/EmailRandomNames")
+                //RequestUri = new Uri("http://localhost:49551/EmailRandomNames")
             };
 
-
-
             Console.WriteLine($"Sending email: {sendEmail.RequestUri}");
+            using (var response = await client.SendAsync(sendEmail))
+            {
+                response.EnsureSuccessStatusCode();
+            }
 
-            var response = await Policy
-                .HandleResult<HttpResponseMessage>(message => !message.IsSuccessStatusCode)
-                .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2), (result, timeSpan, retryCount, context) =>
-                {
-                    Console.WriteLine($"Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
-                })
-                .ExecuteAsync(() => client.SendAsync(sendEmail));
+
+
+            //Console.WriteLine($"Sending email: {sendEmail.RequestUri}");
+
+            //var response = await Policy
+            //    .HandleResult<HttpResponseMessage>(message => !message.IsSuccessStatusCode)
+            //    .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2), (result, timeSpan, retryCount, context) =>
+            //    {
+            //        Console.WriteLine($"Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+            //    })
+            //    .ExecuteAsync(() => client.SendAsync(sendEmail));
 
             //if (response.IsSuccessStatusCode)
             //    Console.WriteLine("Response was successful.");
             //else
             //    Console.WriteLine($"Response failed. Status code {response.StatusCode}");
 
-            response.EnsureSuccessStatusCode();
+            //response.EnsureSuccessStatusCode();
 
 
             //using (var response = await client.SendAsync(sendEmail))
@@ -119,11 +117,11 @@ namespace integration
 
             Console.WriteLine($"Checking emails: {checkEmails.RequestUri}");
 
-            using (var response2 = await client.SendAsync(checkEmails))
+            using (var response = await client.SendAsync(checkEmails))
             {
                 //Console.WriteLine($"Checking emails: {checkEmails.RequestUri}");
 
-                response2.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();
                 var content = await response.Content.ReadAsStringAsync();
                 var messages = JObject.Parse(content);
 
